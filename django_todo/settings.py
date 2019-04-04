96c33954edfbfb83a72fcab2414110fe583f7794
Tapiwa Divine <tchipatiko@yahoo.com>
2019-04-04 13:22:42 +0000

    Implement environments specific values for the DEBUG and DATABASE

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '5z8)7pr__3alj&3ib%s60hs)7zi$e+q!689=4op(xd=zc%=0sx'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = development
